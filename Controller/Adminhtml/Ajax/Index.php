89f0019cb7619730a81a17d8a6b619a17e44e10d
Andreas von Studnitz <avs@integer-net.de>
2016-04-09 16:33:35 -0700

    Add basic adminhtml controller

---
+++ Controller/Adminhtml/Ajax/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Hackathon\AdminFavorites\Controller\Adminhtml\Ajax;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * say admin text
+     */
+    public function execute()
+    {
+        die("Admin");
+    }
+}
