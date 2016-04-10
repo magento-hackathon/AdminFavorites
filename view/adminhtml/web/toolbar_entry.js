1665016d897d58d3a898d9482d83b9f0e735e2e3
jmroz@degdigital <jmroz@degdigital.com>
2016-04-10 09:53:50 -0700

    Move favorites list from admin menu item to toolbar

--- view/adminhtml/web/toolbar_entry.js
+++ view/adminhtml/web/toolbar_entry.js
@@ -5,45 +5,6 @@ define([
 ], function ($) {
     'use strict';
 
-    var favoritesMenu = $("#menu-hackathon-adminfavorites-favorites .submenu [role='menu']")
-    favoritesMenu.html("");
-    favoritesMenu.append('<li class="column">' +
-        '<ul role="menu">' +
-        '<li data-ui-id="menu-magento-reports-report-marketing" class="item-report-marketing  parent  level-1" role="menu-item"><strong class="submenu-group-title" role="presentation">' +
-        '<span>Favorites</span>' +
-        '</strong>' +
-        '<div class="submenu">' +
-        '<ul id="favorites-menu" role="menu">' +
-        '</ul>' +
-        '</div>' +
-        '</li>' +
-        '</ul>' +
-        '</li>');
-    favoritesMenu.append('<li class="column">' +
-        '<ul role="menu">' +
-        '<li data-ui-id="menu-magento-reports-report-marketing" class="item-report-marketing  parent  level-1" role="menu-item"><strong class="submenu-group-title" role="presentation">' +
-        '<span>Mostly Viewed</span>' +
-        '</strong>' +
-        '<div class="submenu">' +
-        '<ul id="mostly-viewed-menu" role="menu">' +
-        '</ul>' +
-        '</div>' +
-        '</li>' +
-        '</ul>' +
-        '</li>');
-    favoritesMenu.append('<li class="column">' +
-        '<ul role="menu">' +
-        '<li data-ui-id="menu-magento-reports-report-marketing" class="item-report-marketing  parent  level-1" role="menu-item"><strong class="submenu-group-title" role="presentation">' +
-        '<span>Recently Viewed</span>' +
-        '</strong>' +
-        '<div class="submenu">' +
-        '<ul id="recently-viewed-menu" role="menu">' +
-        '</ul>' +
-        '</div>' +
-        '</li>' +
-        '</ul>' +
-        '</li>');
-
     var json = '{"is_favorite":0,"my_favorites":[{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/12\/","label":"Test"}],"recently_viewed":[{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/14\/","label":"Test"},{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/14\/param\/1\/","label":"Test"},{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/13\/","label":"Test"},{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/12\/","label":"Test"}],"mostly_viewed":[{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/14\/param\/1\/","label":"Test"},{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/12\/","label":"Test"},{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/13\/","label":"Test"},{"url":"http:\/\/adminfavorites.local\/admin_jz5czk\/test\/abc\/14\/","label":"Test"}]}';
     var parsedJson = JSON.parse(json);
     var populateFavorites = function(json){
@@ -90,4 +51,19 @@ define([
     populateFavorites(parsedJson.my_favorites);
     populateMostlyViewed(parsedJson.mostly_viewed);
     populateRecentlyViewed(parsedJson.recently_viewed);
+
+    var showFavoriteDetails = function (favoriteEntry) {
+        var favorite = favoriteEntry.find('.notifications-entry-description');
+        favorite.addClass('_show');
+    };
+
+
+    $('.favorites-wrapper .admin__action-dropdown-menu .notifications-entry').on('click.showNotification', function (event) {
+        // hide notification dropdown
+        $('.favorites-wrapper .notifications-icon').trigger('click.dropdown');
+
+        showFavoriteDetails($(this));
+        event.stopPropagation();
+    });
+
 });
