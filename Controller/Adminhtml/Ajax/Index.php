0d35aa18472308e89370cd051aab6e1871ccf76d
Andreas von Studnitz <avs@integer-net.de>
2016-04-09 19:26:46 -0700

    Add database write on AJAX Calls

--- Controller/Adminhtml/Ajax/Index.php
+++ Controller/Adminhtml/Ajax/Index.php
@@ -1,8 +1,20 @@
 <?php
 namespace Hackathon\AdminFavorites\Controller\Adminhtml\Ajax;
 
+use Magento\TestFramework\Inspection\Exception;
+
 class Index extends \Magento\Backend\App\Action
 {
+    /**
+     * @var \Hackathon\AdminFavorites\Model\FavoriteFactory
+     */
+    protected $_favoriteFactory;
+
+    /**
+     * @var \Magento\Backend\Model\Auth\Session
+     */
+    protected $_authSession;
+
     /**
      * @var \Magento\Framework\Controller\Result\RawFactory
      */
@@ -10,13 +22,19 @@ class Index extends \Magento\Backend\App\Action
 
     /**
      * @param \Magento\Backend\App\Action\Context $context
+     * @param \Hackathon\AdminFavorites\Model\FavoriteFactory $favoriteFactory
+     * @param \Magento\Backend\Model\Auth\Session $authSession
      * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
+        \Hackathon\AdminFavorites\Model\FavoriteFactory $favoriteFactory,
+        \Magento\Backend\Model\Auth\Session $authSession,
         \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
     ) {
         parent::__construct($context);
+        $this->_favoriteFactory = $favoriteFactory;
+        $this->_authSession = $authSession;
         $this->resultRawFactory = $resultRawFactory;
     }
 
@@ -25,8 +43,33 @@ class Index extends \Magento\Backend\App\Action
      */
     public function execute()
     {
+        $url = $this->getRequest()->getParam('url');
+        if (!$url) {
+            throw new Exception('URL not given!');
+        }
+
+        $label = $this->getRequest()->getParam('label');
+        if (!$label) {
+            throw new Exception('Label not given!');
+        }
+
+        $userId = $this->_authSession->getUser()->getId();
+
+        /** @var \Hackathon\AdminFavorites\Model\Favorite $favorite */
+        $favorite = $this->_favoriteFactory->create();
+        
+        $favorite->addData([
+            'url' => $url,
+            'user_id' => $userId,
+            'label' => $label,
+            'updated_at' => new \Zend_Db_Expr('NOW()'),
+            'number_visits' => intval($favorite->getData('number_visits')) + 1,
+        ]);
+        
+        $favorite->save();
+        
         $output = [
-            'is_favorite' => 1,
+            'is_favorite' => intval($favorite->getData('is_favorite')),
         ];
         
         /** @var \Magento\Framework\Controller\Result\Raw $resultRaw */
